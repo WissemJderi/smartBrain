ba0d6d4c1d7d5cc48671286db79102af06428733
Wissem Jderi <asqr43641@gmail.com>
2025-03-28 23:45:08 +0100

    implement input handling and submission for ImageLinkForm component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 import ImageLinkForm from "./components/imagelinkform/ImageLinkForm.JSX";
 import Logo from "./components/logo/Logo";
@@ -5,14 +6,25 @@ import Navigation from "./components/navigation/Navigation";
 import Rank from "./components/rank/Rank";
 import ParticlesBg from "particles-bg";
 function App() {
+  const [input, setInput] = useState();
+
+  function onInputChange(event) {
+    console.log(event.target.value);
+  }
+
+  function onButtonSubmit() {
+    console.log("Click");
+  }
   return (
     <>
       <ParticlesBg type="cobweb" bg={true} num={20} />
       <Navigation />
       <Logo />
       <Rank />
-      <ImageLinkForm />
-
+      <ImageLinkForm
+        onInputChange={onInputChange}
+        onButtonSubmit={onButtonSubmit}
+      />
       {/*     <FaceRecognition /> */}
     </>
   );
--- src/components/imagelinkform/ImageLinkForm.jsx
+++ src/components/imagelinkform/ImageLinkForm.jsx
@@ -1,18 +1,20 @@
-function ImageLinkForm() {
+function ImageLinkForm({ onInputChange, onButtonSubmit }) {
   return (
     <div>
       <p className="f5"></p>
-      <div
-      // style={{
-      //   display: "flex",
-      //   flexDirection: "column",
-      //   alignItems: "center",
-      //   gap: "10px",
-      // }}
-      >
+      <div>
         <div className="pa4 br3 shadow-5 center form">
-          <input className="f4 pa2 w-70 center" type="text" />
-          <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple ">
+          <input
+            className="f4 pa2 w-70 center"
+            type="text"
+            onChange={(e) => {
+              onInputChange(e);
+            }}
+          />
+          <button
+            className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple "
+            onClick={onButtonSubmit}
+          >
             Detect
           </button>
         </div>
